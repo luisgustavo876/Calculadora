d4110d9f82a373e1dade9a6927e68250a4a53a3d
“DiogoP”luisgustavo876 <luisgustavoferreira2006@gmail.com>
2023-06-25 17:50:25 -0300

    index feito

---
+++ CALC/capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'CALC',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
