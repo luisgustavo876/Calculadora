bb0e3b83164020204beaf031a70334eaca2ac7ab
“DiogoP”luisgustavo876 <luisgustavoferreira2006@gmail.com>
2023-06-25 22:48:33 -0300

    quase pronta

--- CALC/src/app/home/home.page.ts
+++ CALC/src/app/home/home.page.ts
@@ -15,48 +15,65 @@ export class HomePage {
   constructor() {}
 
   //-- função para tratar numeros
+
   clickBotao(parametro: string){
     this.valor = Number(""+this.valor+this.ponto+parametro);
     this.ponto=""
   }
 
   //-- função para +/- e ,
+  
   clickOutros(parametro: string){
+
     if (parametro == "+-") {
       this.valor = (this.valor * -1);
-    } else if (parametro == ",") {
+    } 
+    
+    else if (parametro == ",") {
+
       if (!(String(this.valor).indexOf(".")>0)) {
         this.ponto=".";
       }
-    } else if (parametro == "C"){
+    } 
+    
+    else if (parametro == "C"){
       this.valor = 0;
       this.memoria = 0;
       this.operacao = "";
       this.ponto = "";
     }
+
   }
 
   calcula(tipo:string) {
+
     if (this.operacao == "") {
       this.memoria=this.valor;
     }
+
     else if (this.operacao == "+") {
       this.memoria = this.memoria + this.valor;
     }
+
     else if (this.operacao == "-") {
       this.memoria = this.memoria - this.valor;
     }
+
     else if (this.operacao == "X") {
       this.memoria = this.memoria * this.valor;
     }
+
     else if (this.operacao == "/") {
-      this.memoria = this.memoria / this.valor ;
+      this.memoria = this.memoria / this.valor ;      //  
     }
+
     if (tipo=="=") {
       this.valor=this.memoria;
       this.memoria=0;
       this.operacao="";
-    } else {
+    } 
+    
+    else {
     this.operacao=tipo;
     this.valor=0;
     }
