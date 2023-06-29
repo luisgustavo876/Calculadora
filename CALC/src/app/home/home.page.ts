bb97d1492b042580ddc2275c037056bef38e8c79
“DiogoP”luisgustavo876 <luisgustavoferreira2006@gmail.com>
2023-06-29 13:49:11 -0300

    COMIT COMENTADO

--- CALC/src/app/home/home.page.ts
+++ CALC/src/app/home/home.page.ts
@@ -7,10 +7,11 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  valor: number = 0;
-  memoria: number = 0;
-  operacao: string = "";
-  ponto: string = "";
+  valor: number = 0;      //
+  memoria: number = 0;    //declaraçao de variavel
+  operacao: string = "";  //
+  ponto: string = "";     //
+  resultado: number = 0;   
 
 
   constructor() {}
@@ -22,80 +23,82 @@ export class HomePage {
     this.ponto=""
   }
 
-  //-- função para +/- e ,
+  //-- função para +/- e , e C e backspace
   
   clickOutros(parametro: string){
 
-    if (parametro == "+-") {
-      this.valor = (this.valor * -1);
-    } 
+    if (parametro == "+-") {            //
+      this.valor = (this.valor * -1);   // se o parametro for +- pega o valor que vc digitou e multplica por -1 mudando o sinal
+    }                                   //
     
-    else if (parametro == ",") {
-
-      if (!(String(this.valor).indexOf(".")>0)) {
-        this.ponto=".";
-      }
-    } 
+    else if (parametro == ",") {                   //
+                                                   //
+      if (!(String(this.valor).indexOf(".")>0)) {  // se o parametro for , entao o valor diferente de string sera adicionado
+        this.ponto=".";                            // um . que é o parametro que faz oq a virgula faz na programaçao
+      }                                            // e tambem tranforma o . criado em um numero menor que 0 descendo uma classe gramatical
+    }                                              //
     
-    else if (parametro == "C"){
-      this.valor = 0;
-      this.memoria = 0;
-      this.operacao = "";
-      this.ponto = "";
+    else if (parametro == "C"){   // 
+      this.valor = 0;             //
+      this.memoria = 0;           // se o parametro for C ela zerará tudo conseguentemente apagando os dados da telinha
+      this.operacao = "";         //
+      this.ponto = "";            //
     }
 
-    else if (parametro == "--") {
-        this.valor = this.valor = 0;
-    }
-
-    else if (parametro == "%") {
-        this.valor = this.valor /100 ;
-  }
-
+    else if (parametro == "--") {     //
+        this.valor = this.valor  = 0; //se o parametro for -- zera o valor zerando a parte debaixo da conta
+    }                                 //
   }
   
+    //-- função para parametros de conta
 
   calcula(tipo:string) {
 
-    if (this.operacao == "") {
-      this.memoria=this.valor;
+    if (this.operacao == "") {  //se  parametro for null o valor digitado sobe para a memoria ja prontro para receber o sinal
+      this.memoria=this.valor;  //
     }
 
     else if (this.operacao == "+") {
-      this.memoria = this.memoria + this.valor ;
+      this.memoria = this.memoria + this.valor ;  // se  o parametro for + ele pega a memoria e faz a soma com o valor digitado
     }
 
     else if (this.operacao == "-") {
-      this.memoria = this.memoria - this.valor;
+      this.memoria = this.memoria - this.valor; // se  o parametro for - ele pega a memoria e faz a subtraçao com o valor digitado
     }
 
     else if (this.operacao == "X") {
-      this.memoria = this.memoria * this.valor;
+      this.memoria = this.memoria * this.valor; // se  o parametro for X ele pega a memoria e faz a multiplicaçao com o valor digitado
     }
 
-    else if (this.operacao == "/") {
-      this.memoria = this.memoria / this.valor ;     
+    else if (this.operacao == "/") {  
+      this.memoria = this.memoria / this.valor ;  // se  o parametro for / ele pega a memoria e faz a divisao com o valor digitado
     }
 
     else if (this.operacao == "X²") {
-      this.memoria = this.memoria ** this.valor;     
+      this.memoria = this.memoria ** this.valor;   // se  o parametro for X2 ele pega a memoria e faz a potenciaçao com o valor digitado
       // if (this.operacao == "X²"){
       //   this.calcula+this.clickBotao+this.clickOutros === (this.tipo=="=")
       // } 
     }
 
     else if (this.operacao == "√") {
-      this.memoria = Math.sqrt(this.memoria);     
+      this.memoria = Math.sqrt(this.memoria);  // se  o parametro for √ ele pega a memoria e faz a radiciaçao com o valor digitado
     }
 
-    if (tipo=="=") {
-      this.valor=this.memoria;
-      this.memoria=0;
-      this.operacao="";
+    else if (this.operacao == "%") {
+      this.memoria = this.memoria / 100;         // se  o parametro for % ele pega a memoria e faz a porcentagem com a memoria 
+      this.memoria = this.memoria * this.valor;  // e depois multiplica pelo valor digitado
+
+  }
+
+    if (tipo=="=") {            //
+      this.valor=this.memoria;  // se  o parametro for = ele pega o valor e da igual a memoria e zera a memoria e as operaçoes
+      this.memoria=0;           //
+      this.operacao="";         //
     } 
     
     else {
-    this.operacao=tipo;
+    this.operacao=tipo; //
     this.valor=0;
     }
   }
